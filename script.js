ae86daf3adcb3c1838a3210d6ac2b76b3b877e09
Antoine <32277387+Amanite2012@users.noreply.github.com>
2025-06-27 21:17:01 +0000

    First push
    Signed-off-by: Antoine <32277387+Amanite2012@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,15 @@
+
+console.log("Portfolio script loaded.");
+
+document.querySelectorAll('header nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
